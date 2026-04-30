ae40fa1ce39d0f6ba1e3792410ac168ca1b88844
Bryan Behrenshausen <bryan.behrenshausen@sas.com>
2026-04-30 10:12:24 -0400

    fix: update baseUrl and GitHub link in website config
    Signed-off-by: Bryan Behrenshausen <bryan.behrenshausen@sas.com>

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://sassoftware.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/<projectName>/",
+  baseUrl: "/viyactl/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -61,7 +61,7 @@ const config: Config = {
       },
       items: [
         {
-          href: "https://github.com/sassoftware/<projectName>",
+          href: "https://github.com/sassoftware/viyactl",
           className: "header-github-link",
           title: "GitHub repository",
           position: "right",
